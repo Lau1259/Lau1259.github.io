55c4b6370929c298590030eb5b951f26ca9b2271
Lau1259 <62459638+Lau1259@users.noreply.github.com>
2020-11-18 14:40:01 -0300

    Initial Commit

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,13 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=703bf38fa0ac6aec6fd04dd0b7e37720";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
